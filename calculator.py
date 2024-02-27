19659956953dc9b4e738f934ce1c770d9a6718ff
Agnes <agnesajema@gmail.com>
2024-02-27 21:19:48 +0300

    add file

---
+++ calculator.py
@@ -0,0 +1,8 @@
+ # prompt for user input
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+
+# perform operation)
+print("Difference: {} - {} = {}".format(a,b,a-b))
