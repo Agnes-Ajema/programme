1fac89b59155ada4c060740579faf18915f2977f
Agnes <agnesajema@gmail.com>
2024-02-27 22:31:35 +0300

    add files

--- calculator.py
+++ calculator.py
@@ -6,3 +6,6 @@ b = float(input("Enter the second number: "))
 
 # perform operation)
 print("Difference: {} - {} = {}".format(a,b,a-b))
+print("Sum: {} + {} = {}".format(a,b,a+b))
+print("Product: {} * {} = {}".format(a,b,a*b))
+print("Quotient: {} / {} = {}".format(a,b,a/b))
